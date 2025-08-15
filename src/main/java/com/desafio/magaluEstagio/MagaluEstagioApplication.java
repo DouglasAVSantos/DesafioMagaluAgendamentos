cd50d8dc98b4b673713896aff4c80c2dbcb5e76a
DouglasAVSantos <104694849+DouglasAVSantos@users.noreply.github.com>
2025-08-14 21:30:08 -0300

    subindo o restante do projeto.

---
+++ src/main/java/com/desafio/magaluEstagio/MagaluEstagioApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.magaluEstagio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagaluEstagioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagaluEstagioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/magaluEstagio/MagaluEstagioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.magaluEstagio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagaluEstagioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
